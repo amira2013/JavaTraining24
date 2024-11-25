5bc5543742a143ea892c77faf9ac28a233674d4c
STUDENTLOANER <drisschima42@gmail.com>
2024-11-24 21:02:21 -0500

    This the second commit of sum calcul

---
+++ Calculsum.java
@@ -0,0 +1,12 @@
+public class Calculsum {
+    public static void main(String[] args) {
+        int sum=0;
+        int i;
+        for (i=0;i<5;i++){
+            sum+=i;
+        }
+        System.out.println("The sum is "+sum);
+    }
+
+
+}
