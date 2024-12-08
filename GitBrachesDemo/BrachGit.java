ea4c74f864d5486a0214208a5bd991d4964ce36a
amira2013 <drisschima42@gmail.com>
2024-12-07 20:53:10 -0500

    This is the first commit in main

---
+++ GitBrachesDemo/BrachGit.java
@@ -0,0 +1,8 @@
+package GitBrachesDemo;
+
+public class BrachGit {
+    public static void main(String[] args) {
+        System.out.println("My kids are beatiful");
+        System.out.println(6*8);
+    }
+}
